50500e87bd32cbf50983cc0594ca6c10052c0cad
Kirill Shevtsov <kirwork.em@gmail.com>
2020-08-30 17:46:10 +0300

    Changing root reducer import

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
-import rootReducer from '../reducers/rootReducer';
+import createRootReducer from '../reducers/rootReducer';
 
 const initialState = {};
 const enhancers = [];
@@ -10,6 +10,6 @@ export const history = createBrowserHistory();
 
 const composedEnhancers = compose(applyMiddleware(routerMiddleware(history)), ...enhancers);
 
-const store = createStore(rootReducer, initialState, composedEnhancers);
+const store = createStore(createRootReducer(history), initialState, composedEnhancers);
 
 export default store;
