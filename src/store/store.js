c0d66371008143ecb673fe37503683938d41c7e4
Kirill Shevtsov <kirwork.em@gmail.com>
2020-08-30 16:33:06 +0300

    Adding root reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/rootReducer';
 
 const initialState = {};
 const enhancers = [];
 const composedEnhancers = compose(applyMiddleware(), ...enhancers);
 
-const store = createStore(initialState, composedEnhancers);
+const store = createStore(rootReducer, initialState, composedEnhancers);
 
 export default store;
