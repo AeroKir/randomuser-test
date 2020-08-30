feba38faf9ec5903e26f75ee9bfe89b7bd699f16
Kirill Shevtsov <kirwork.em@gmail.com>
2020-08-30 17:05:12 +0300

    Creating browser history

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,14 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
 import rootReducer from '../reducers/rootReducer';
 
 const initialState = {};
 const enhancers = [];
-const composedEnhancers = compose(applyMiddleware(), ...enhancers);
+
+export const history = createBrowserHistory();
+
+const composedEnhancers = compose(applyMiddleware(routerMiddleware(history)), ...enhancers);
 
 const store = createStore(rootReducer, initialState, composedEnhancers);
 
