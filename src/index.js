e592e7ca723d05ea9cc801f66067d2d8dfed9617
Kirill Shevtsov <kirwork.em@gmail.com>
2020-08-30 17:41:21 +0300

    Adding ConnectedRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store/store';
+import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch } from 'react-router-dom';
+
+import store, { history } from './store/store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="*" render={() => (<h1>PAGE NOT FOUND</h1>)} />
+      </Switch>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
