9fb548e30ec9ee24b2f80fa89740dc7bc4d47948
Kirill Shevtsov <kirwork.em@gmail.com>
2020-08-31 20:23:51 +0300

    Adding BrowserRouter

--- src/layouts/base/BaseAppLayout.jsx
+++ src/layouts/base/BaseAppLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout } from 'antd';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import classNames from 'classnames';
 
 import Header from '../../components/header/Header';
@@ -9,6 +9,7 @@ import Footer from '../../components/footer/Footer';
 import HomePage from '../../pages/home/HomePage';
 import UserProfilePage from '../../pages/user-profile/UserProfilePage';
 import ContactsPage from '../../pages/contacts/ContactsPage';
+import NotFoundPage from '../../pages/not-found/NotFoundPage';
 
 import './BaseAppLayout.css';
 
@@ -16,16 +17,21 @@ function BaseAppLayout() {
   const layout = classNames('BaseAppLayout');
 
   return (
-    <Layout className={layout}>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/profile" component={UserProfilePage} />
-        <Route path="/contacts" component={ContactsPage} />
-        <Route path="*" render={() => (<h1>PAGE NOT FOUND</h1>)} />
-      </Switch>
-      <Footer />
-    </Layout>
+    <BrowserRouter>
+      <Layout className={layout}>
+        <Header />
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/profile" component={UserProfilePage} />
+          <Route path="/contacts" component={ContactsPage} />
+          <Route path="*" component={NotFoundPage} />
+        </Switch>
+
+        <Footer />
+      </Layout>
+    </BrowserRouter>
+
   );
 }
 
