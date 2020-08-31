2e81ae775dda20aff8e68d009ab70570f80edc4e
Kirill Shevtsov <kirwork.em@gmail.com>
2020-08-31 20:20:10 +0300

    Removing unused styles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import BaseAppLayout from './layouts/base/BaseAppLayout';
 
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <>
       <BaseAppLayout />
-    </div>
+    </>
   );
 }
 
