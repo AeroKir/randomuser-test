3ca31993ca6fa98084bf3d137000978aaa3c9264
Kirill Shevtsov <kirwork.em@gmail.com>
2020-08-31 20:18:52 +0300

    Adding signIn form

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Layout, Button, Row, Col,
 } from 'antd';
@@ -7,36 +7,51 @@ import classNames from 'classnames';
 
 import Logo from '../logo/Logo';
 import Navbar from '../navbar/Navbar';
+import SignInForm from '../forms/SignInForm';
 import UserProfileHeaderBlock from '../user/UserProfileHeaderBlock';
 
 import 'antd/dist/antd.css';
 import './Header.css';
 
 function Header() {
+  const [isSignInFormVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isSignInFormVisible);
   const header = classNames('Header');
 
   return (
-    <Layout.Header className={header}>
+    <>
+      <Layout.Header className={header}>
 
-      <Row align="top" style={{ width: '5%' }}>
-        <Col>
-          <Logo />
-        </Col>
-      </Row>
-      <Row justify="space-between" align="top" style={{ width: '95%' }}>
-        <Col span={10}>
-          <Navbar disabled />
-        </Col>
-        <Col span={2}>
-          <Button type="link" htmlType="button" icon={<LoginOutlined />}>Sign In</Button>
-        </Col>
-        {/* <Col span={5}>
-          <UserProfileHeaderBlock />
-        </Col> */}
-      </Row>
+        <Row align="top" style={{ width: '5%' }}>
+          <Col>
+            <Logo />
+          </Col>
+        </Row>
+        <Row justify="space-between" align="top" style={{ width: '95%' }}>
+          <Col span={10}>
+            <Navbar />
+          </Col>
+          <Col span={2}>
+            <Button
+              type="link"
+              htmlType="button"
+              icon={(
+                <LoginOutlined />
+              )}
+              onClick={toggle}
+            >
+              Sign In
+            </Button>
+          </Col>
+          {/* <Col span={5}>
+            <UserProfileHeaderBlock />
+          </Col> */}
+        </Row>
 
-    </Layout.Header>
+      </Layout.Header>
 
+      <SignInForm isVisible={isSignInFormVisible} handleFormClose={toggle} />
+    </>
   );
 }
 
