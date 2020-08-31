7b412bfdc26253e241c18e48da71ac23e9cd392d
Kirill Shevtsov <kirwork.em@gmail.com>
2020-08-31 20:17:14 +0300

    Changin className

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout } from 'antd';
 
+import './HomePage.css';
 import reactLogo from '../../assets/react-logo.svg';
 
 function HomePage() {
@@ -8,8 +9,8 @@ function HomePage() {
 
   return (
     <Content>
-      <div>
-        <img src={reactLogo} alt="" />
+      <div className="HomePage">
+        <img className="App-logo" src={reactLogo} alt="React logo" />
       </div>
     </Content>
   );
