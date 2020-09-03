71314388d4775bf984c294c54976244b3d604cb2
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-04 01:09:30 +0300

    Adding contactsPageReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import { reducer as formReducer } from 'redux-form';
 import signInFormReducer from './signInFormReducer';
+import contactsPageReducer from './contactsPageReducer';
 
 const createRootReducer = (history) => combineReducers({
   router: connectRouter(history),
   form: formReducer,
   signInFormReducer,
+  contactsPageReducer,
 });
 
 export default createRootReducer;
