bb091a6425078446af8a571f9fa1fd953eb775c0
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-03 21:33:49 +0300

    Adding actions

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,2 +1,4 @@
 export const SHOW_SIGN_IN_FORM = 'SHOW_SIGN_IN_FORM';
 export const HIDE_SIGN_IN_FORM = 'HIDE_SIGN_IN_FORM';
+export const GET_USER = 'GET_USER';
+export const GET_USER_AUTH_DATA = 'GET_USER_AUTH_DATA';
