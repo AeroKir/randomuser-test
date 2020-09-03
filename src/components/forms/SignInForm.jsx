df4cae4202ecdc2a0e37d55e5fbed62d96870eda
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-03 21:33:20 +0300

    Adding form validation

--- src/components/forms/SignInForm.jsx
+++ src/components/forms/SignInForm.jsx
@@ -1,28 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { reduxForm, Field, formValueSelector } from 'redux-form';
+import { TextField } from 'redux-form-antd';
+import { Modal, Button } from 'antd';
 import {
-  Modal,
-  Button,
-  Form,
-  Input,
-} from 'antd';
-import { CloseOutlined, LockOutlined, UserOutlined } from '@ant-design/icons';
+  CloseOutlined, LockOutlined, UserOutlined, EyeOutlined, EyeInvisibleOutlined,
+} from '@ant-design/icons';
 
-function SignInForm({ isVisible, handleFormClose }) {
+const emailRequired = (value) => (value || typeof value === 'number' ? undefined : 'The email field is required.');
+const passwordRequired = (value) => (value || typeof value === 'number' ? undefined : 'The password field is required.');
+const email = (value) => (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+  ? 'The email format is invalid.'
+  : undefined);
+const minLength = (min) => (value) => (value && value.length < min ? `The password must be at least ${min} characters.` : undefined);
+const minLength8 = minLength(8);
+
+let SignInForm = ({
+  isVisible, handleFormSubmit, handleFormClose, isFormValid, invalid,
+}) => {
+  const onClickAction = (e) => {
+    e.preventDefault();
+    handleFormSubmit(e);
+  };
+
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(!passwordShown);
+  };
+
+  const eyeVisible = <EyeOutlined onClick={togglePasswordVisiblity} />;
+  const eyeInvisible = <EyeInvisibleOutlined onClick={togglePasswordVisiblity} />;
+
   return (
     <Modal
       title="Sign In"
       centered
       visible={isVisible}
       closable={false}
-      footer={[
+      footer={null}
+    >
+      <form onSubmit={onClickAction}>
+        <div style={{ marginBottom: '15px' }}>
+          <Field
+            name="userEmail"
+            type="email"
+            component={TextField}
+            size="large"
+            placeholder="Email"
+            prefix={<UserOutlined />}
+            validate={[emailRequired, email]}
+          />
+        </div>
+        <div style={{ marginBottom: '15px' }}>
+          <Field
+            name="userPassword"
+            type={passwordShown ? 'text' : 'password'}
+            placeholder="Password"
+            component={TextField}
+            visibilityToggle
+            size="large"
+            prefix={<LockOutlined />}
+            suffix={passwordShown ? eyeVisible : eyeInvisible}
+            validate={[passwordRequired, minLength8]}
+          />
+        </div>
+
         <Button
           key="signin"
           type="primary"
           htmlType="submit"
+          disabled={invalid || isFormValid}
         >
           Sign In
-        </Button>,
+        </Button>
         <Button
           key="cancel"
           type="text"
@@ -31,40 +82,33 @@ function SignInForm({ isVisible, handleFormClose }) {
           onClick={handleFormClose}
         >
           Cancel
-        </Button>,
-      ]}
-    >
-      <Form>
-        <Form.Item
-          rules={[{ required: true, message: 'The email field is required.' }]}
-          extra="Type any valid email"
-        >
-          <Input
-            name="email"
-            size="large"
-            placeholder="Email"
-            prefix={<UserOutlined />}
-          />
-        </Form.Item>
-        <Form.Item
-          rules={[{ required: true, message: 'The password field is required.' }]}
-          extra="Type any valid password"
-        >
-          <Input.Password
-            name="password"
-            size="large"
-            placeholder="Password"
-            prefix={<LockOutlined />}
-          />
-        </Form.Item>
-      </Form>
+        </Button>
+      </form>
+
     </Modal>
   );
-}
+};
 
 SignInForm.propTypes = {
   isVisible: PropTypes.bool.isRequired,
+  handleFormSubmit: PropTypes.func.isRequired,
   handleFormClose: PropTypes.func.isRequired,
+  isFormValid: PropTypes.bool.isRequired,
+  invalid: PropTypes.bool.isRequired,
 };
 
-export default SignInForm;
+const selector = formValueSelector('signInForm');
+
+SignInForm = connect((state) => {
+  const userEmail = selector(state, 'userEmail');
+  const userPassword = selector(state, 'userPassword');
+
+  return {
+    userEmail,
+    userPassword,
+  };
+})(SignInForm);
+
+export default reduxForm({
+  form: 'signInForm',
+})(SignInForm);
