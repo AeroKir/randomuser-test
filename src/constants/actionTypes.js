426733cd4f7e9e13779c5b666b1f028ff4ccb91f
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-04 01:13:38 +0300

    Adding action types

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -2,3 +2,8 @@ export const SHOW_SIGN_IN_FORM = 'SHOW_SIGN_IN_FORM';
 export const HIDE_SIGN_IN_FORM = 'HIDE_SIGN_IN_FORM';
 export const GET_USER = 'GET_USER';
 export const GET_USER_AUTH_DATA = 'GET_USER_AUTH_DATA';
+export const SWITCH_TO_USER_PROFILE = 'SWITCH_TO_USER_PROFILE';
+
+// Contacts page actions
+export const SET_TILED_VIEW = 'SET_TILED_VIEW';
+export const SET_TABULAR_VIEW = 'SET_TABULAR_VIEW';
