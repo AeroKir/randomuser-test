23723ad53797b02c0be9c8ece7e958bcac62ccc2
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-03 21:36:00 +0300

    Adding fetchUser and getUserAuthData actions

--- src/actions/signInForm.js
+++ src/actions/signInForm.js
@@ -1,5 +1,41 @@
 import createAction from './createAction';
-import { SHOW_SIGN_IN_FORM, HIDE_SIGN_IN_FORM } from '../constants/actionTypes';
+import {
+  SHOW_SIGN_IN_FORM, HIDE_SIGN_IN_FORM, GET_USER, GET_USER_AUTH_DATA,
+} from '../constants/actionTypes';
 
 export const showSignInForm = createAction(SHOW_SIGN_IN_FORM);
 export const hideSignInForm = createAction(HIDE_SIGN_IN_FORM);
+
+export function getUser(userData) {
+  return {
+    type: GET_USER,
+    payload: { userData },
+  };
+}
+
+export function getUserAuthData(email, password) {
+  return {
+    type: GET_USER_AUTH_DATA,
+    payload: { email, password },
+  };
+}
+
+export function fetchUser() {
+  return (dispatch, getState) => {
+    const state = getState();
+
+    const { form: { signInForm: { values: { userEmail, userPassword } } } } = state;
+
+    dispatch(getUserAuthData(userEmail, userPassword));
+
+    const url = `https://randomuser.me/api/?seed=${userEmail}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        const userData = json;
+        return userData;
+      })
+      .then((userData) => dispatch(getUser(userData)));
+  };
+}
