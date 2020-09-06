040f260c48518078e234df1e7d3c007235b508b6
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-06 21:07:14 +0300

    Adding thunk and logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import initialState from './initialState';
@@ -8,7 +10,7 @@ const enhancers = [];
 
 export const history = createBrowserHistory();
 
-const composedEnhancers = compose(applyMiddleware(routerMiddleware(history)), ...enhancers);
+const composedEnhancers = compose(applyMiddleware(routerMiddleware(history), logger, thunk), ...enhancers);
 
 const store = createStore(createRootReducer(history), initialState, composedEnhancers);
 
