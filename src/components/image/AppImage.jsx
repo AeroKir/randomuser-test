56e6f5084b30d0d3385493bf09081a5aa04e9981
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-06 21:15:28 +0300

    Adding PropTypes validation

--- src/components/image/AppImage.jsx
+++ src/components/image/AppImage.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Image } from 'antd';
 import classNames from 'classnames';
 
-import img from '../../assets/panda.jpg';
-
-function AppImage({ className }) {
+function AppImage({ src, className }) {
   const imageClasses = classNames(className);
   return (
     <Image
-      src={img}
+      src={src}
       className={imageClasses}
     />
   );
 }
+
+AppImage.propTypes = {
+  src: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+AppImage.defaultProps = {
+  className: '',
+};
 
 export default AppImage;
