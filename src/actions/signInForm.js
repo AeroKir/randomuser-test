6a7ca4061cc334e6a09e0f550be1697a65fb1157
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-06 20:57:20 +0300

    Dispatch data loading

--- src/actions/signInForm.js
+++ src/actions/signInForm.js
@@ -1,10 +1,18 @@
+import { push } from 'connected-react-router';
+
 import createAction from './createAction';
 import {
-  SHOW_SIGN_IN_FORM, HIDE_SIGN_IN_FORM, GET_USER, GET_USER_AUTH_DATA,
+  SHOW_SIGN_IN_FORM,
+  HIDE_SIGN_IN_FORM,
+  GET_USER, GET_USER_AUTH_DATA,
+  SWITCH_TO_USER_PROFILE,
+  USER_DATA_LOADING,
 } from '../constants/actionTypes';
 
 export const showSignInForm = createAction(SHOW_SIGN_IN_FORM);
 export const hideSignInForm = createAction(HIDE_SIGN_IN_FORM);
+export const switchToUserProfile = createAction(SWITCH_TO_USER_PROFILE);
+export const userDataLoading = createAction(USER_DATA_LOADING);
 
 export function getUser(userData) {
   return {
@@ -20,22 +28,34 @@ export function getUserAuthData(email, password) {
   };
 }
 
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
 export function fetchUser() {
   return (dispatch, getState) => {
     const state = getState();
 
     const { form: { signInForm: { values: { userEmail, userPassword } } } } = state;
 
+    dispatch(userDataLoading());
     dispatch(getUserAuthData(userEmail, userPassword));
 
     const url = `https://randomuser.me/api/?seed=${userEmail}`;
 
     fetch(url)
+      .then(handleErrors)
       .then((response) => response.json())
       .then((json) => {
-        const userData = json;
-        return userData;
+        dispatch(getUser(json.results));
+        return json;
       })
-      .then((userData) => dispatch(getUser(userData)));
+      .then(() => {
+        dispatch(push('/profile'));
+      })
+      .catch(() => { });
   };
 }
