f8bdb912b608a71c6dd83b473094bfa350af17cb
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-06 21:48:52 +0300

    Renaming action

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -2,7 +2,7 @@ export const SHOW_SIGN_IN_FORM = 'SHOW_SIGN_IN_FORM';
 export const HIDE_SIGN_IN_FORM = 'HIDE_SIGN_IN_FORM';
 export const GET_USER = 'GET_USER';
 export const GET_USER_AUTH_DATA = 'GET_USER_AUTH_DATA';
-export const USER_DATA_LOADING = 'USER_DATA_LOADING'; 
+export const USER_DATA_LOADING = 'USER_DATA_LOADING';
 export const SWITCH_TO_USER_PROFILE = 'SWITCH_TO_USER_PROFILE';
 export const LOGOUT = 'LOGOUT';
 
