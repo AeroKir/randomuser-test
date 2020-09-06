b284a0fe671eb7dcdf5c672a21a2ddb83aa5d6d8
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-06 21:35:03 +0300

    Adding contacts shape

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -7,6 +7,12 @@ const initialState = {
     userEmail: '',
     userPassword: '',
   },
+  contacts: {
+    isTableView: true,
+    isTiledView: false,
+    isLoading: false,
+    contactsCollection: JSON.parse(localStorage.getItem('contactsCollection')) || {},
+  },
 };
 
 export default initialState;
