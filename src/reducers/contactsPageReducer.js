5e0564c46cd67e4e1f9c3dcd30977b9709987839
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-07 14:23:57 +0300

    Adding get contacts cases

--- src/reducers/contactsPageReducer.js
+++ src/reducers/contactsPageReducer.js
@@ -1,5 +1,11 @@
 import initialState from '../store/initialState';
-import { SET_TILED_VIEW, SET_TABLE_VIEW } from '../constants/actionTypes';
+import {
+  SET_TILED_VIEW,
+  SET_TABLE_VIEW,
+  CONTACTS_LOADING,
+  GET_CONTACTS_SUCCESS,
+  CONTACTS_LOADING_FAIL,
+} from '../constants/actionTypes';
 
 function contactsPageReducer(state = initialState.contacts, action) {
   switch (action.type) {
@@ -19,6 +25,29 @@ function contactsPageReducer(state = initialState.contacts, action) {
         isTiledView: false,
         isTableView: true,
       };
+    }
+
+    case CONTACTS_LOADING:
+    {
+      return {
+        ...state,
+        isLoading: true,
+      };
+    }
+
+    case GET_CONTACTS_SUCCESS:
+    {
+      localStorage.setItem('contactsCollection', JSON.stringify(action.payload.contactsData));
+
+      return {
+        ...state,
+        contactsCollection: action.payload.contactsData,
+        isLoading: false,
+
+        // isUserDataLoading: false,
+      };
+
+      // return { ...state, fetchedUser: action.payload.userData };
     }
 
     default:
