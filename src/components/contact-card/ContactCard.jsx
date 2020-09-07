e2d63508e0c54a00236ffb5cd8d59d40c7c36662
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-07 14:10:39 +0300

    Adding props and PropTypes valodation

--- src/components/contact-card/ContactCard.jsx
+++ src/components/contact-card/ContactCard.jsx
@@ -1,72 +1,91 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Card,
   Typography,
   Divider,
+  Tag,
 } from 'antd';
 import classNames from 'classnames';
 
-import Badge from '../badge/Badge';
 import AppImage from '../image/AppImage';
 
 import 'antd/dist/antd.css';
 import './ContactCard.css';
 
-function ContactCard() {
+function ContactCard({
+  contactCardImage,
+  contactCardTitle,
+  contactCardTitleSecondary,
+  contactCardEmail,
+  contactCardPhone,
+  contactCardLocation,
+  contactCardNationality,
+}) {
   const {
     Title, Text,
   } = Typography;
 
   const userContactsList = classNames('UserProfile-userContactsList');
   const userContactsItem = classNames('UserProfile-userContactsItem');
-  const text = {
-    values: [
-      {
-        email: 'example@mail.com',
-        phone: '+7789716293',
-        location: '6655 Bollinger Rd, Cape Fear, Tennessee 49132',
-      },
-    ],
-  };
 
   return (
 
     <Card
       hoverable
-      cover={<AppImage />}
+      cover={<AppImage src={contactCardImage} />}
     >
       <Title level={3} style={{ textAlign: 'left' }}>
-        Mr. Herbert Phillips
+        {contactCardTitle}
         <Text
           type="secondary"
           style={{ fontSize: '19px', paddingLeft: '5px' }}
         >
-          (41 years)
+          {contactCardTitleSecondary}
         </Text>
       </Title>
       <Divider dashed style={{ margin: '12px 0' }} />
 
-      {text.values.map((i) => (
-        <ul className={userContactsList}>
-          <li className={userContactsItem}>
-            <Text copyable={{ text: i.email }} style={{ paddingRight: '8px' }} />
-            <a href={`mailto:${i.email}`}>{i.email}</a>
-          </li>
-          <li className={userContactsItem}>
-            <Text copyable={{ text: i.phone }} style={{ paddingRight: '8px' }} />
-            <a href={`tel:${i.phone}`}>{i.phone}</a>
-          </li>
-          <li className={userContactsItem}>
-            <Text copyable={{ text: i.location }} style={{ paddingRight: '8px' }} />
-            <span>{i.location}</span>
-          </li>
-        </ul>
-      ))}
+      <ul className={userContactsList}>
+        <li className={userContactsItem}>
+          <Text copyable={{ text: contactCardEmail }} style={{ paddingRight: '8px' }} />
+          <a href={`mailto:${contactCardEmail}`}>{contactCardEmail}</a>
+        </li>
+        <li className={userContactsItem}>
+          <Text copyable={{ text: contactCardPhone }} style={{ paddingRight: '8px' }} />
+          <a href={`tel:${contactCardPhone}`}>{contactCardPhone}</a>
+        </li>
+        <li className={userContactsItem}>
+          <Text copyable={{ text: contactCardLocation }} style={{ paddingRight: '8px' }} />
+          <span>{contactCardLocation}</span>
+        </li>
+      </ul>
+
       <Divider dashed style={{ margin: '12px 0' }} />
-      <Badge />
+      <Tag>{contactCardNationality}</Tag>
     </Card>
 
   );
 }
+
+ContactCard.propTypes = {
+  contactCardImage: PropTypes.string,
+  contactCardTitle: PropTypes.string,
+  contactCardTitleSecondary: PropTypes.string,
+  contactCardEmail: PropTypes.string,
+  contactCardPhone: PropTypes.string,
+  contactCardLocation: PropTypes.string,
+  contactCardNationality: PropTypes.string,
+};
+
+ContactCard.defaultProps = {
+  contactCardImage: '',
+  contactCardTitle: 'Card title',
+  contactCardTitleSecondary: 'title secondary',
+  contactCardEmail: 'mail@example.com',
+  contactCardPhone: '1234567',
+  contactCardLocation: 'Earth',
+  contactCardNationality: 'Geek',
+};
 
 export default ContactCard;
