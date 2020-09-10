7fc8299629be7bb7d6b600f3226ccd9bb25ca308
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-10 20:18:32 +0300

    Adding new fields

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -11,10 +11,15 @@ const initialState = {
     isTableView: true,
     isTiledView: false,
     isLoading: false,
-    contactsCollection: JSON.parse(localStorage.getItem('contactsCollection')) || {},
+    contactsCollection: JSON.parse(localStorage.getItem('contactsCollection')) || [],
+    collectionLength: JSON.parse(localStorage.getItem('collectionLength')) || 0,
+    males: JSON.parse(localStorage.getItem('malesAmount')) || 0,
+    females: JSON.parse(localStorage.getItem('femalesAmount')) || 0,
+    indeterminate: JSON.parse(localStorage.getItem('indeterminateAmount')) || 0,
     contactsPerPage: 10,
     defaultCurrentPage: 1,
     currentPage: 1,
+    isContactProfileLoading: false,
   },
 };
 
