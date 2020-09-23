5c0c8f16ac8136e088a302b2a6ee280a8a141850
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-23 19:18:20 +0300

    Adding mapStateToProps and clear form action

--- src/components/forms/contactsSearchFilterFormContainer.js
+++ src/components/forms/contactsSearchFilterFormContainer.js
@@ -1,15 +1,28 @@
 import { connect } from 'react-redux';
-import { filterByGender, filterByNationality, filterByName } from '../../actions/contactsPage';
+import {
+  filterByGender,
+  filterByNationality,
+  filterByName,
+  clearFiltersForm,
+} from '../../actions/contactsPage';
 import ContactsSearchFilterForm from './ContactsSearchFilterForm';
+
+function mapStateToProps({ contacts }) {
+  const { isContactFilterClearDisabled } = contacts;
+  return {
+    isContactFilterClearDisabled,
+  };
+}
 
 function mapDispatchToProps(dispatch) {
   return {
     genderFilter: (genderValue) => dispatch(filterByGender(genderValue)),
     nationalityFilter: (nationalityValue) => dispatch(filterByNationality(nationalityValue)),
     nameFilter: (nameValue) => dispatch(filterByName(nameValue)),
+    handleClearForm: () => dispatch(clearFiltersForm()),
   };
 }
 
-const ContactsSearchFilterFormContainer = connect(null, mapDispatchToProps)(ContactsSearchFilterForm);
+const ContactsSearchFilterFormContainer = connect(mapStateToProps, mapDispatchToProps)(ContactsSearchFilterForm);
 
 export default ContactsSearchFilterFormContainer;
