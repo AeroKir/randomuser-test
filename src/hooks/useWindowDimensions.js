740da21e5b3f418018c4d7c98e1738767e757a33
Kirill Shevtsov <kirwork.em@gmail.com>
2020-09-24 22:19:04 +0300

    Creating a custom hook

---
+++ src/hooks/useWindowDimensions.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import getWindowDimensions from '../utils/getWindowDimensions';
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+export default useWindowDimensions;
